891a80c7b02caca4d10221517c1bef9bcfad1a61
godmayer-jdi <tig774ra@gmail.com>
2025-09-25 23:27:38 +0400

    Проведено тестирование. После проверки покрытия (<65%) тесты доработаны для выполнения условий покрытия. Проведена повторная проверка линтерами, указанные ошибки устранены

--- tests/test_product.py
+++ tests/test_product.py
@@ -1,7 +1,7 @@
-from src.product import Product, Category, CategoryIterator
+from src.product import Category, CategoryIterator, Product
 
 
-def test_product_price_getter_setter():
+def test_product_price_getter_setter() -> None:
     p = Product("Хлеб", "Свежий хлеб", 50.0, 10)
     assert p.price == 50.0
 
@@ -13,20 +13,20 @@ def test_product_price_getter_setter():
     assert p.price == 60.0
 
 
-def test_product_str():
+def test_product_str() -> None:
     p = Product("Молоко", "Свежее", 70.5, 5)
     expected_str = "Молоко, 70.5 руб. Остаток: 5 шт."
     assert str(p) == expected_str
 
 
-def test_product_add():
+def test_product_add() -> None:
     p1 = Product("Молоко", "Свежее", 70, 5)  # 350
     p2 = Product("Хлеб", "Свежий", 50, 10)  # 500
     total = p1 + p2
     assert total == 850
 
 
-def test_category_add_product_and_str():
+def test_category_add_product_and_str() -> None:
     cat = Category("Продукты", "Категория продуктов")
     p1 = Product("Яблоко", "Свежие", 40, 15)
     p2 = Product("Банан", "Свежие", 60, 5)
@@ -42,27 +42,21 @@ def test_category_add_product_and_str():
     assert str(cat) == expected_str
 
 
-def test_category_iterator():
+def test_category_iterator() -> None:
     p1 = Product("Яблоко", "Свежие", 40, 15)
     p2 = Product("Банан", "Свежие", 60, 5)
     cat = Category("Фрукты", "Сладкие")
     cat.add_product(p1)
     cat.add_product(p2)
 
-    # Итерация по CategoryIterator
     iter_obj = CategoryIterator(cat)
     products = list(iter_obj)
     assert products == [p1, p2]
 
 
-def test_product_new_product_method():
+def test_product_new_product_method() -> None:
     products_list = [Product("Яблоко", "Свежие", 40, 10)]
-    new_prod_dict = {
-        "name": "Яблоко",
-        "description": "Зеленое",
-        "price": 50,
-        "quantity": 5
-    }
+    new_prod_dict = {"name": "Яблоко", "description": "Зеленое", "price": 50, "quantity": 5}
     prod = Product.new_product(new_prod_dict, products_list)
 
     assert prod.quantity == 15  # Количество увеличилось
